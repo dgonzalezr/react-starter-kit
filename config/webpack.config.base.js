6a5cd5a2d21f64300f16a11b5b507b333447120c
Dabiel González Ramos <dabiel.ramos@endava.com>
2019-02-12 22:39:38 +0200

    chore: webpack configurations code formatting

--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -4,55 +4,55 @@ const merge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = env => {
-    const { PLATFORM, VERSION } = env;
-    return merge([
-        {
-            module: {
-                rules: [
-                    {
-                        test: /\.(js|jsx)$/,
-                        exclude: /node_modules/,
-                        use: {
-                            loader: 'babel-loader',
-                            options: {
-                                // This is a feature of `babel-loader` for webpack (not Babel itself).
-                                // It enables caching results in ./node_modules/.cache/babel-loader/
-                                // directory for faster rebuilds.
-                                cacheDirectory: true,
-                                plugins: ['react-hot-loader/babel']
-                            }
-                        }
-                    },
-                    {
-                        test: /\.(png|jpe?g|gif|svg|webp)$/i,
-                        use: {
-                            loader: require.resolve('file-loader'),
-                            options: {
-                                name: 'static/media/[name].[ext]'
-                            }
-                        }
-                    },
-                    {
-                        test: /\.(ttf|eot|woff2?)$/i,
-                        use: {
-                            loader: require.resolve('file-loader'),
-                            options: {
-                                name: 'static/fonts/[name].[ext]'
-                            }
-                        }
-                    }
-                ]
-            },
-            plugins: [
-                new HtmlWebpackPlugin({
-                    template: './public/index.html',
-                    filename: './index.html'
-                }),
-                new webpack.DefinePlugin({
-                    'process.env.VERSION': JSON.stringify(env.VERSION),
-                    'process.env.PLATFORM': JSON.stringify(env.PLATFORM)
-                })
-            ]
-        }
-    ]);
+  const { PLATFORM, VERSION } = env;
+  return merge([
+    {
+      module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                // This is a feature of `babel-loader` for webpack (not Babel itself).
+                // It enables caching results in ./node_modules/.cache/babel-loader/
+                // directory for faster rebuilds.
+                cacheDirectory: true,
+                plugins: ['react-hot-loader/babel']
+              }
+            }
+          },
+          {
+            test: /\.(png|jpe?g|gif|svg|webp)$/i,
+            use: {
+              loader: require.resolve('file-loader'),
+              options: {
+                name: 'static/media/[name].[ext]'
+              }
+            }
+          },
+          {
+            test: /\.(ttf|eot|woff2?)$/i,
+            use: {
+              loader: require.resolve('file-loader'),
+              options: {
+                name: 'static/fonts/[name].[ext]'
+              }
+            }
+          }
+        ]
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+          template: './public/index.html',
+          filename: './index.html'
+        }),
+        new webpack.DefinePlugin({
+          'process.env.VERSION': JSON.stringify(env.VERSION),
+          'process.env.PLATFORM': JSON.stringify(env.PLATFORM)
+        })
+      ]
+    }
+  ]);
 };
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -15,42 +15,41 @@ const baseConfig = require('./webpack.config.base');
 const resolveApp = relativePath => path.resolve(fs.realpathSync(process.cwd()), relativePath);
 
 const prodConfiguration = env =>
-    merge([
-        {
-            mode: 'production',
-            output: {
-                // The build folder.
-                path: resolveApp('dist'),
-                // Generated JS file names (with nested folders).
-                // One main bundle, and one file per asynchronous chunk.
-                filename: 'static/js/[name].[chunkhash:8].js',
-                chunkFilename: 'static/js/[name].[chunkhash:8].chunk.js',
-                // TODO: We inferred the "public path" (such as / or /my-project) from homepage.
-                // publicPath: publicPath,
-                // Point sourcemap entries to original disk location (format as URL on Windows)
-                devtoolModuleFilenameTemplate: info =>
-                    path.relative(resolveApp('src'), info.absoluteResourcePath).replace(/\\/g, '/')
-            },
-            optimization: {
-                runtimeChunk: 'single',
-                splitChunks: {
-                    cacheGroups: {
-                        vendor: {
-                            test: /[\\/]node_modules[\\/]/,
-                            name: 'vendors',
-                            chunks: 'all'
-                        }
-                    }
-                },
-                minimizer: [new UglifyJsPlugin()]
-            },
-            plugins: [
-                // new OptimizeCssAssetsPlugin(),
-                new Visualizer({
-                    filename: './statistics.html'
-                })
-            ]
-        }
-    ]);
+  merge([
+    {
+      mode: 'production',
+      output: {
+        // The build folder.
+        path: resolveApp('dist'),
+        // Generated JS file names (with nested folders).
+        // One main bundle, and one file per asynchronous chunk.
+        filename: 'static/js/[name].[chunkhash:8].js',
+        chunkFilename: 'static/js/[name].[chunkhash:8].chunk.js',
+        // Point sourcemap entries to original disk location (format as URL on Windows)
+        devtoolModuleFilenameTemplate: info =>
+          path.relative(resolveApp('src'), info.absoluteResourcePath).replace(/\\/g, '/')
+        // TODO: inferred the "public path"
+      },
+      optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+          cacheGroups: {
+            vendor: {
+              test: /[\\/]node_modules[\\/]/,
+              name: 'vendors',
+              chunks: 'all'
+            }
+          }
+        },
+        minimizer: [new UglifyJsPlugin()]
+      },
+      plugins: [
+        // new OptimizeCssAssetsPlugin(),
+        new Visualizer({
+          filename: './statistics.html'
+        })
+      ]
+    }
+  ]);
 
 module.exports = env => merge(baseConfig(env), prodConfiguration(env));
